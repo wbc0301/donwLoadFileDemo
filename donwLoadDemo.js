0efcd423d554425fc32a94f646adcf68b1b6ba41
wbc <417780894@qq.com>
2020-08-20 16:28:56 +0800

    上一版应为原生ajax请求xls没有设置responseType=blob,所以失败，此版修复OK

--- donwLoadDemo.js
+++ donwLoadDemo.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const server = http.createServer((req, res) => {
   let url = req.url;
 
-  if(url === '/') {
+  if (url === '/') {
     res.setHeader('Content-Type', 'text/html;charset=utf-8')
     fs.readFile('index.html', function (err, data) {
       if (err) {
@@ -12,14 +12,25 @@ const server = http.createServer((req, res) => {
       }
       res.end(data);
     });
-  }else if(url === '/getXLS') {
+  } else if (url === '/axios') {
+    res.setHeader('Content-Type', 'application/javascript;charset=utf-8')
+    fs.readFile('axios.js', function (err, data) {
+      if (err) {
+        return console.log(err);
+      }
+      res.end(data);
+    });
+  } else if (url === '/getXLS') {
     res.setHeader('Content-Type', 'application/vnd.ms-excel;charset=utf-8')
     fs.createReadStream('t.xls').pipe(res);
-  }else if(url === '/getHTML') {
+  } else if (url === '/getHTML') {
     res.setHeader('Content-Type', 'text/html;charset=utf-8')
     fs.createReadStream('t.html').pipe(res);
+  } else if (url === '/apiDemo') {
+    let data = { code: 123, message: 'success!!!', data: { name: 'zs', age: 18 } }
+    res.end(JSON.stringify(data));
   }
-  
+
 })
 
 server.listen(3000)
